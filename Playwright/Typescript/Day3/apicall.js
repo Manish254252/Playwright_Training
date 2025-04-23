8c5abad427a38839a31335cc3e817757753b4229
Manish Solanki <70832573+Manish254252@users.noreply.github.com>
2025-04-23 11:11:40 +0530

    Code removed

--- Playwright/Typescript/Day3/apicall.js
+++
@@ -1,4 +0,0 @@
-function getFacts(url) {
-    return fetch(url).then(function (res) { return res.json(); });
-}
-getFacts("https://cataas.com/cat?width=200;height=200;json=true").then(function (data) { return console.log(data); });
--- Playwright/Typescript/Day3/apicall.ts
+++
@@ -1,5 +0,0 @@
-function getFacts(url: string): Promise<Response> {
-    return fetch(url).then(res => res.json());
-}
-
-getFacts("https://cataas.com/cat?width=200;height=200;json=true").then(data => console.log(data));
